1dfafa7f183ab3c373677d04d0fb7a8c4435b213
Martin Aedma <N2942@student.jamk.fi>
2021-09-12 10:16:29 +0300

    start

---
+++ portfolio/pages/Navbar.js
@@ -0,0 +1,25 @@
+import * as React from 'react'
+
+const Navbar = () => {
+
+    return <nav>
+        <div className='flex-center navbar-container'>
+            <ul>
+                <li>
+                    HOME
+                </li>
+                <li>
+                    ABOUT
+                </li>
+                <li>
+                    PROJECTS
+                </li>
+                <li>
+                    GAMEDEV
+                </li>
+            </ul>
+        </div>
+    </nav>
+}
+
+export default Navbar
---
+++ portfolio/pages/index.js
@@ -0,0 +1,24 @@
+import Navbar from "./Navbar"
+
+export default function Home() {
+  return (
+    <div>
+      <div className='flex-center header-container'>
+
+          <h1>Martin</h1>
+
+      </div>
+
+      <Navbar/>
+
+
+      <main>
+
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
