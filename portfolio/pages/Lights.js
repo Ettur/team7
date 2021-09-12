eefa5813630d85e946b28d53bf74c02e57dec7d6
Martin Aedma <N2942@student.jamk.fi>
2021-09-12 17:24:16 +0300

    added

---
+++ portfolio/pages/Lights.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Lights = () => {
+    return (
+        <>
+            <ambientLight intensity={0.5}/>
+        </>
+    )
+}
+
+export default Lights
--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,4 +1,10 @@
-import Navbar from "./Navbar"
+import React, { useRef, useState } from 'react'
+import { Suspense } from 'react'
+import ReactDOM from 'react-dom'
+import { Canvas, useFrame } from '@react-three/fiber'
+import Navbar from './Navbar'
+import Lights from './Lights'
+import Model from './TestModel'
 
 export default function Home() {
   return (
@@ -13,6 +19,13 @@ export default function Home() {
 
 
       <main>
+
+        <Canvas colorManagement camera={{ position: [0,1,5]}} className=''>
+          <Lights/>
+          <Suspense fallback={null}>
+            <Model position={[0,0,0]}/>
+          </Suspense>
+        </Canvas>
 
       </main>
 
