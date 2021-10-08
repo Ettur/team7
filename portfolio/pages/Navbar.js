e77dec7ab085bf2a7c29ee4134490903362b4e2e
Martin Aedma <N2942@student.jamk.fi>
2021-10-08 08:36:06 +0300

    updates

--- portfolio/pages/Navbar.js
+++ portfolio/pages/Navbar.js
@@ -4,24 +4,26 @@ const Navbar = (props) => {
 
     const style = () => {
         if (props.showNav) {
-            return 'flex-center navbar-container navbar-container-row'
+            return 'flex-center navbar-container navbar-container-show'
+        } else {
+            return 'flex-center navbar-container'
         }
     }
 
     return <nav>
-       <div className='flex-center navbar-container'>
+       <div className={style()}>
             <ul>
-                <li>
+                <li style={{borderBottom: 'none', borderTopRightRadius:'3px', borderTopLeftRadius:'3px'}}>
                     HOME
                 </li>
-                <li>
-                    ABOUT
+                <li style={{borderBottom: 'none'}}>
+                    PROJECT
                 </li>
-                <li>
-                    PROJECTS
+                <li style={{borderBottom: 'none'}}>
+                    TEAM
                 </li>
-                <li>
-                    GAMEDEV
+                <li style={{borderBottomRightRadius:'3px', borderBottomLeftRadius:'3px'}}>
+                    BLOG
                 </li>
             </ul>
         </div>
