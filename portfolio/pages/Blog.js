963c9e3c5d5f8ba6b769206187c20c13fe11a3ed
unknown <martin.aedma@liveto.io>
2021-10-10 15:22:54 +0300

    updates

--- portfolio/pages/Blog.js
+++ portfolio/pages/Blog.js
@@ -4,6 +4,14 @@ import Header from './Header'
 const Blog = () => {
     return <div className='blog-container' id='blog'>
         <Header headline='NEWS' info='LATEST UPDATES'/>
+        <div className='latest-news'>
+            <div className='latest-news-text'>
+                <h2>ENGINES READY!</h2>
+                <span>6.10.2021</span>
+                <p>Team has been assembled, Prepearing for project start!</p>
+            </div>
+            <img src='together.jpg'/>
+        </div>
     </div>
 }
 export default Blog
