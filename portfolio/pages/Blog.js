18e742807974d939350843925be6cc1969382b8e
Martin Aedma <N2942@student.jamk.fi>
2021-10-10 23:13:01 +0300

    updates

--- portfolio/pages/Blog.js
+++ portfolio/pages/Blog.js
@@ -1,16 +1,29 @@
 import * as React from 'react'
 import Header from './Header'
+import { useWindowSize } from '../util/functions'
 
 const Blog = () => {
+
+    const size = useWindowSize()
+    const containerWidth = 95 - (size.width / 30) - 5
+    const marginLeft = () => {
+        const calculated = size.width / 100
+        if (size.width > 1000) {
+            return 0
+        } else {
+            return calculated
+        }
+    }
+
     return <div className='blog-container' id='blog'>
         <Header headline='NEWS' info='LATEST UPDATES'/>
-        <div className='latest-news'>
-            <div className='latest-news-text'>
-                <h2>ENGINES READY!</h2>
+        <div className='latest-news' style={{marginLeft : marginLeft() + '%'}}>
+            <div className='latest-news-text' style={{ width: containerWidth + '%'}}>
                 <span>6.10.2021</span>
-                <p>Team has been assembled, Prepearing for project start!</p>
+                <h2>ENGINE REVVING!</h2>
+                <p>Team has been assembled, project start imminent!</p>
             </div>
-            <img src='together.jpg'/>
+            <img src='together.jpg' style={{ width: containerWidth + '%'}}/>
         </div>
     </div>
 }
